4d1ba556385df643aace2417bf910b415cf9267e
pirangy <pirangy.ariel@gmail.com>
2024-03-18 13:39:16 -0300

    Config: first commit

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+import { getAllListingsRoute } from "./getAllListings";
+import { getListingRoute } from "./getListing";
+import { addViewToListingRoute } from "./addViewToListing";
+import { getUserListingsRote } from "./getUserListings";
+import { createNewListingRoute } from "./createNewListing";
+import { updateListingRoute } from "./updateListing";
+import { deleteListingRoute } from "./deleteListing";
+
+export default [
+    getAllListingsRoute,
+    getListingRoute,
+    addViewToListingRoute,
+    getUserListingsRote,
+    createNewListingRoute,
+    updateListingRoute,
+    deleteListingRoute,
+];
